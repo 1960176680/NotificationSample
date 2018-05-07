09eb950a7fa851f25bed1b620a676c573a0074cd
Sun <1960176680@qq.com>
2018-05-07 11:28:34 +0800

    提交
    提交

--- app/src/main/java/com/zhouwenguang/hz/notificationsample/MainActivity.java
+++ app/src/main/java/com/zhouwenguang/hz/notificationsample/MainActivity.java
@@ -1,13 +1,53 @@
 package com.zhouwenguang.hz.notificationsample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.btn1);
+//        Notification的创建
+        final NotificationCompat.Builder builder=new NotificationCompat.Builder(MainActivity.this,"1");
+        builder.setSmallIcon(R.drawable.ic_stat_name);
+        builder.setContentTitle("通知您");
+        builder.setContentText("今晚一起吃饭，好吗？");
+//        PendingIntent的创建
+        final NotificationManager notificationManager= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(ResultActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+/**
+ *在通知中显示进度
+ */
+
+
+
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationManager.notify(1,builder.build());
+            }
+        });
     }
 }
